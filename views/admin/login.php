172c6b71f003b7a7d2c97b2868aa8159ebf58a3a
Xiaolin Jin Lin <x140l1n@hotmail.com>
2021-10-25 13:56:16 +0200

    Add directory admin, landingPage into media directory. Styling admin panel.

---
+++ views/admin/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/admin.css"/>
+    <title>Login - Panel admin OpenHouseJocs</title>
+</head>
+<body class="vh-100">
+    <div class="container-fluid h-100">
+        <div class="row h-100">
+            <div class="col-lg-6 login-background">
+
+            </div>
+            <div class="col-lg-6">
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
