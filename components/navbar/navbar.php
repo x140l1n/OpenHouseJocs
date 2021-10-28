5c8d26219e4c91c4d91064671b79f47f2761fec4
Pau Monedero Roigé <pmonederor2021@cepnet.net>
2021-10-28 09:43:38 +0200

    icons included
    Fixed icons problem in fotter (because of the absolute path)

--- components/navbar/navbar.php
+++ components/navbar/navbar.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="navbar.css">
-  <title>OpenhouseJocsNavbar</title>
-</head>
-
-<body>
-
-  <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
     <div class="container-fluid">
 
       <a class="navbar-brand" href="#">
@@ -53,10 +40,6 @@
       </div>
 
     </div>
-  </nav>
+</nav>
 
- 
-</body>
-
-</html>
 
