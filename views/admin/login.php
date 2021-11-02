8617d837b176d5da23f2677556cb42d153f9d32c
Xiaolin Jin Lin <x140l1n@hotmail.com>
2021-11-02 11:01:46 +0100

    Login 90%.

--- views/admin/login.php
+++ views/admin/login.php
@@ -1,10 +1,19 @@
+<?php 
+
+session_start();
+
+if (isset($_SESSION['id'])) {
+    header("Location: dashboard.php");
+}
+
+?>
 <!DOCTYPE html>
 <html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" coadd ntent="width=device-width, initial-scale=1.0">
     <?php
     //Import links.
     require("./partials/links.html");
