2e3c5828d6667f9865b44e487354b55eb28a2391
Xiaolin Jin Lin <x140l1n@hotmail.com>
2021-11-02 10:53:24 +0100

    90% Login.

--- views/admin/assets/js/login.js
+++ views/admin/assets/js/login.js
@@ -5,10 +5,58 @@ document.addEventListener("DOMContentLoaded", init);
  * @param {Event} e  Contains a number of properties that describe the event that occurred
  */
 function init(e) {
-    //Fire the event switch darkMode when clicked the icon.
-    let element = e.target;
-    let iconDarkMode = element.querySelector("#switchDarkMode + i");
-    iconDarkMode.addEventListener("click", function () {
-        element.querySelector("#switchDarkMode").click();
+    //Get the element target.
+    let document = e.target;
+
+    //Fire the event switch dark mode when clicked the icon.
+    let iconDarkMode = document.querySelector("#switchDarkMode + i");
+    iconDarkMode.addEventListener("click", function (e) {
+        document.querySelector("#switchDarkMode").click();
+    });
+
+    //Check the input email.
+    let inputEmail = document.querySelector("#input-email");
+    inputEmail.addEventListener("input", function (e) {
+        let input = e.target;
+        let errorLabel = document.querySelector(input.getAttribute("error-label"));
+
+        if (input.value.trim()) {
+            //From functions.js.
+            if (validateEmail(input.value)) {
+                input.classList.remove("border-danger");
+                input.classList.add("border-success");
+                errorLabel.classList.add("invisible");
+                errorLabel.innerText = "Error";
+            } else {
+                input.classList.add("border-danger");
+                input.classList.remove("border-success");
+                errorLabel.classList.remove("invisible");
+                errorLabel.innerText = "El email no es válido.";
+            }
+        } else {
+            input.classList.remove("border-success");
+            input.classList.remove("border-danger");
+            errorLabel.classList.add("invisible");
+            errorLabel.innerText = "Error";
+        }
+    });
+
+    //Event toggle password.
+    let togglePassword = document.querySelector("#toggle-password");
+    togglePassword.addEventListener("click", function (e) {
+        let element = e.currentTarget;
+
+        let inputPassword = document.querySelector(element.getAttribute("input-toggle"));
+
+        if (equalsIgnoreCase(inputPassword.getAttribute("type"), "password")) {
+            inputPassword.setAttribute("type", "text");
+            element.firstChild.classList.toggle("fa-eye");
+            element.firstChild.classList.toggle("fa-eye-slash");
+        } else {
+            inputPassword.setAttribute("type", "password");
+            element.firstChild.classList.toggle("fa-eye");
+            element.firstChild.classList.toggle("fa-eye-slash");
+        }
     });
 }
+
