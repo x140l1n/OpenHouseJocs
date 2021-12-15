f6187f6a2b0b5d24d2590868f4cc32ff07f88d35
Isipisi00 <gonzalezaparicio.2000@gmail.com>
2021-12-15 10:38:45 +0100

    game image

--- views/gameSelect/gameSelect.php
+++ views/gameSelect/gameSelect.php
@@ -8,11 +8,22 @@
     <link rel="stylesheet" href="gameSelect.css">
 
 </head>
-<body>
-    <div class="container mt-sm-4">
-        <img class="gSelect_img mx-auto d-block img-fluid" src="../../media/gameSelectViews/jugar.gif" alt="">
+<body>    
+
+    <div class="mt-sm-4">
+        <img class="gSelect_main mx-auto d-block img-fluid" src="../../media/gameSelectViews/jugar.gif" alt="">
     </div>
-    <div class="row d-flex align-items-center">
+
+    <div class="title">
+        <h2>Bienvenido, Juanito</h2>
+        <p>Selecciona ciclo</p>
+        <select>
+            <option>DAW</option>
+            <option>DAM</option>
+        </select>
+    </div>
+
+    <div class="justify-content-around row d-flex align-items-center">
         <div class="col-sm-12 col-lg-3 gSelect_img">
             <img class="gSelect_img" src="../../media/gameSelectViews/arkanoid_gif.gif" alt="">
         </div>
@@ -26,6 +37,8 @@
             <img class="gSelect_img" src="../../media/gameSelectViews/space_gif.gif">
         </div>
     </div>
+   
+
 </body>
 </html>
 
